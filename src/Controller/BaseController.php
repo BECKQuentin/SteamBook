5d830d3725bf00c94368f51d834792c9d4c659d1
BECKQuentin <beckquentin26@gmail.com>
2021-08-02 11:48:35 +0200

    mise en place

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BaseController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(): Response
+    {
+        return $this->render('base/home.html.twig', [
+        ]);
+    }
+
+    public function header($routeName)
+    {        
+        return $this->render('base/_header.html.twig', [ 
+            'route_name' => $routeName
+        ]);  
+    }
+}
