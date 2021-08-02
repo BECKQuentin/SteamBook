3685601a116a30f3842d6cc7b94ed4b9c2d733a4
BECKQuentin <beckquentin26@gmail.com>
2021-08-02 16:14:50 +0200

    'login'

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -13,7 +13,17 @@ class BaseController extends AbstractController
      */
     public function home(): Response
     {
+        $json = file_get_contents('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=97F647085699DE84DDB8E41A4A5F829A&steamids=76561198109840983&format=json');
+
+        $obj = json_decode($json, true);
+
+        $profil = $obj['response']['players']['0'];
+
+        $games = file_get_contents('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=97F647085699DE84DDB8E41A4A5F829A&steamid=76561197960434622&format=json&include_appinfo=1');
+    
         return $this->render('base/home.html.twig', [
+            'profil' => $profil,            
+            'games' => $games
         ]);
     }
 
